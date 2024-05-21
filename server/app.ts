fbbf1fd0cec1a7a53f21d2344551aa52a8ddc533
CharlieAl <charlie.lerma.ramirez@gmail.com>
2024-05-21 12:15:56 -0700

    add Kinde  for authentication

--- server/app.ts
+++ server/app.ts
@@ -2,15 +2,15 @@ import { Hono } from 'hono'
 import { logger } from 'hono/logger'
 import { expensesRoute } from './routes/expenses'
 import { serveStatic } from 'hono/bun'
+import { authRoute } from './routes/auth'
 const app = new Hono()
 
 app.use('*', logger())
 
-app.get('/test', (c) => {
-  return c.json({ message: 'Hello, World!' })
-})
-
-const ApiRoutes = app.basePath('/api').route('/expenses', expensesRoute)
+const ApiRoutes = app
+  .basePath('/api')
+  .route('/expenses', expensesRoute)
+  .route('/', authRoute)
 
 app.get('*', serveStatic({ root: './frontend/dist' }))
 app.get('/', serveStatic({ path: './frontend/dist/index.html' }))
---
+++ server/routes/auth.ts
@@ -0,0 +1,30 @@
+import { Hono } from 'hono'
+import { getUser, kindeClient, sessionManager } from '../kinde'
+import { URL } from 'url' // Add this import statement
+export const authRoute = new Hono()
+  .get('/login', async (c) => {
+    const loginUrl = await kindeClient.login(sessionManager)
+    return c.redirect(loginUrl.toString())
+  })
+  .get('/register', async (c) => {
+    const registerUrl = await kindeClient.register(sessionManager)
+    return c.redirect(registerUrl.toString())
+  })
+  .get('/callback', async (c) => {
+    // get called eveyr time we login or register
+    const url = new URL(c.req.url)
+    await kindeClient.handleRedirectToApp(sessionManager, url)
+    return c.redirect('/')
+  })
+  .get('/logout', async (c) => {
+    const logoutUrl = await kindeClient.logout(sessionManager)
+    return c.redirect(logoutUrl.toString())
+  })
+  .get('/me', async (c) => {
+    const isAuthenticated = await kindeClient.isAuthenticated(sessionManager)
+    return c.json({ isAuthenticated })
+  })
+  .get('/user', getUser, async (c) => {
+    const user = c.var.user
+    return c.json({ user })
+  })
